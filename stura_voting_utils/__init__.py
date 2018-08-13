df5d7bba903ec07cb06503a1ab403a18f85eed99
Fabian Wenzelmann <fabianwenzelmann@posteo.de>
2018-08-13 22:01:38 +0200

    new version for pypi

--- stura_voting_utils/__init__.py
+++ stura_voting_utils/__init__.py
@@ -2,4 +2,4 @@ from .utils import WeightedVoter, VotingGroup, VotingCollection, MedianVotingSke
 from .parser import ParseException, concurrency_match, parse_voters, parse_voting_collection, parse_csv, parse_concurrency
 
 __title__ = 'stura_voting_utils'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
